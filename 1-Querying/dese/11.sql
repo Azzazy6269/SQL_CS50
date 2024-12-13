fc80948e3e35311e13773ccbc28922436da041b7
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:31:06 +0200

    Update 11.sql

--- 1-Querying/dese/11.sql
+++ 1-Querying/dese/11.sql
@@ -1,1 +1,4 @@
-SELECT schools.name , expenditures.per_pupil_expenditure , graduation_rates.graduated FROM "schools" INNER JOIN "expenditures" ON schools.district_id = expenditures.district_id INNER JOIN "graduation_rates" ON schools.id = graduation_rates.school_id ORDER BY per_pupil_expenditure DESC , schools.name ASC  ;
+SELECT schools.name , expenditures.per_pupil_expenditure , graduation_rates.graduated FROM "schools" 
+  INNER JOIN "expenditures" ON schools.district_id = expenditures.district_id 
+  INNER JOIN "graduation_rates" ON schools.id = graduation_rates.school_id 
+  ORDER BY per_pupil_expenditure DESC , schools.name ASC  ;
