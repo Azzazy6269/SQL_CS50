2749503e018a2c65da7aa95fd9f9eace324c888c
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:27:37 +0200

    Update 4.sql

--- 1-Querying/dese/4.sql
+++ 1-Querying/dese/4.sql
@@ -1,2 +1,3 @@
 
-SELECT "city" , "public_for_each_city" FROM (SELECT "city" , COUNT(*) AS "public_for_each_city" FROM "schools" WHERE "type" = 'Public School' GROUP BY "city") ORDER BY "public_for_each_city" DESC LIMIT 10 ;
+SELECT "city" , "public_for_each_city" FROM (SELECT "city" , COUNT(*) AS "public_for_each_city" FROM "schools"
+  WHERE "type" = 'Public School' GROUP BY "city") ORDER BY "public_for_each_city" DESC LIMIT 10 ;
