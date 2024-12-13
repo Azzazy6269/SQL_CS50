5c4a1e2e51a38088b49dd77bc6b8bb5250cadb9b
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:38:13 +0200

    Update 13.sql

--- 1-Querying/dese/13.sql
+++ 1-Querying/dese/13.sql
@@ -1,1 +1,12 @@
-SELECT schools.name  FROM "schools" LEFT JOIN "graduation_rates" ON schools.id = graduation_rates.school_id WHERE graduation_rates.graduated = 100;
+WITH Averages AS (
+ SELECT AVG(expenditures.per_pupil_expenditure) AS avg_expenditure, AVG(teacher_ratings.percent_exemplary) AS avg_percent_exemplary
+ FROM expenditures
+ INNER JOIN teacher_ratings ON expenditures.district_id = teacher_ratings.district_id
+)
+SELECT districts.name AS district_name, expenditures.per_pupil_expenditure, teacher_ratings.percent_exemplary
+FROM  districts
+INNER JOIN  expenditures ON districts.id = expenditures.district_id
+INNER JOIN  teacher_ratings ON districts.id = teacher_ratings.district_id
+WHERE  expenditures.per_pupil_expenditure > (SELECT avg_expenditure FROM Averages)
+AND teacher_ratings.percent_exemplary > (SELECT avg_percent_exemplary FROM Averages)
+ORDER BY teacher_ratings.percent_exemplary DESC, expenditures.per_pupil_expenditure DESC;
