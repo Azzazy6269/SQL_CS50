320ce8c7ad89a8b1d6ccd08ce2e37ad6d2df8381
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:30:15 +0200

    Update 10.sql

--- 1-Querying/dese/10.sql
+++ 1-Querying/dese/10.sql
@@ -1,1 +1,4 @@
-SELECT districts.name , expenditures.per_pupil_expenditure FROM "districts" INNER JOIN "expenditures" ON districts.id = expenditures.district_id WHERE districts.type = 'Public School District' ORDER BY expenditures.per_pupil_expenditure DESC LIMIT 10 ;
+SELECT districts.name , expenditures.per_pupil_expenditure FROM "districts"
+  INNER JOIN "expenditures" ON districts.id = expenditures.district_id 
+  WHERE districts.type = 'Public School District'
+  ORDER BY expenditures.per_pupil_expenditure DESC LIMIT 10 ;
