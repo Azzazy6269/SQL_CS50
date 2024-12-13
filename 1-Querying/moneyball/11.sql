e2d4a15b77b089597e16b64fb325851469b3c9e2
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:44:16 +0200

    Update 11.sql

--- 1-Querying/moneyball/11.sql
+++ 1-Querying/moneyball/11.sql
@@ -1,1 +1,5 @@
-SELECT players.first_name , players.last_name ,(salaries.salary/NULLIF(performances.H,0)) AS "dollars per hit" FROM players INNER JOIN salaries ON players.id = salaries.player_id INNER JOIN performances ON performances.player_id = players.id AND performances.year = salaries.year WHERE salaries.year = 2001 AND performances.H IS NOT NULL AND salaries.salary IS NOT NULL AND performances.H > 0 ORDER BY "dollars per hit" ASC , players.first_name , players.last_name LIMIT 10 ;
+SELECT players.first_name , players.last_name ,(salaries.salary/NULLIF(performances.H,0)) AS "dollars per hit" FROM players 
+  INNER JOIN salaries ON players.id = salaries.player_id 
+  INNER JOIN performances ON performances.player_id = players.id AND performances.year = salaries.year 
+  WHERE salaries.year = 2001 AND performances.H IS NOT NULL AND salaries.salary IS NOT NULL AND performances.H > 0 
+  ORDER BY "dollars per hit" ASC , players.first_name , players.last_name LIMIT 10 ;
