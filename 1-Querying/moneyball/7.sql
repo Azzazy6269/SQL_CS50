9d782d55e90dec16e9ecad6cb13929e2ac1994ee
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:41:14 +0200

    Update 7.sql

--- 1-Querying/moneyball/7.sql
+++ 1-Querying/moneyball/7.sql
@@ -1,1 +1,3 @@
-SELECT "first_name" , "last_name" FROM "players" INNER JOIN "salaries" ON players.id = salaries.player_id WHERE salaries.salary = (SELECT MAX(salary)FROM salaries);
+SELECT "first_name" , "last_name" FROM "players" 
+  INNER JOIN "salaries" ON players.id = salaries.player_id 
+  WHERE salaries.salary = (SELECT MAX(salary)FROM salaries);
