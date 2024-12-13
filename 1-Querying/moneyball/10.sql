d80e926c8f2158a3e63622300ebcc56e95554803
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:43:05 +0200

    Update 10.sql

--- 1-Querying/moneyball/10.sql
+++ 1-Querying/moneyball/10.sql
@@ -1,1 +1,4 @@
-SELECT players.first_name, players.last_name, salaries.salary, performances.HR AS home_runs, salaries.year FROM players INNER JOIN salaries ON players.id = salaries.player_id INNER JOIN performances ON players.id = performances.player_id AND salaries.year = performances.year ORDER BY players.id ASC, salaries.year DESC, performances.HR DESC, salaries.salary DESC;
+SELECT players.first_name, players.last_name, salaries.salary, performances.HR AS home_runs, salaries.year FROM players 
+  INNER JOIN salaries ON players.id = salaries.player_id 
+  INNER JOIN performances ON players.id = performances.player_id AND salaries.year = performances.year 
+  ORDER BY players.id ASC, salaries.year DESC, performances.HR DESC, salaries.salary DESC;
