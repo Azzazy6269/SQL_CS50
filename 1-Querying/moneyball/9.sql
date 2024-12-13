c3aafa1baa41c85129ae535bb18585701d55fd5e
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:42:11 +0200

    Update 9.sql

--- 1-Querying/moneyball/9.sql
+++ 1-Querying/moneyball/9.sql
@@ -1,1 +1,4 @@
-SELECT teams.name , ROUND(AVG(salaries.salary),2) AS "avg_sal" FROM teams JOIN salaries ON teams.id = salaries.team_id WHERE salaries.year =2001 GROUP BY salaries.team_id ORDER BY avg_sal ASC LIMIT 5 ;
+SELECT teams.name , ROUND(AVG(salaries.salary),2) AS "avg_sal" FROM teams 
+  JOIN salaries ON teams.id = salaries.team_id 
+  WHERE salaries.year =2001 
+  GROUP BY salaries.team_id ORDER BY avg_sal ASC LIMIT 5 ;
