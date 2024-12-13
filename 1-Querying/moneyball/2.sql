27364ce742d2d067134983468c22aaf1e46f4332
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-14 01:39:00 +0200

    Update 2.sql

--- 1-Querying/moneyball/2.sql
+++ 1-Querying/moneyball/2.sql
@@ -1,1 +1,4 @@
-SELECT "salary" , "year" FROM salaries INNER JOIN players ON salaries.player_id = players.id WHERE players.first_name = 'Cal' AND players.last_name = 'Ripken' ORDER BY "year" DESC ;
+SELECT "salary" , "year" FROM salaries 
+  INNER JOIN players ON salaries.player_id = players.id 
+  WHERE players.first_name = 'Cal' AND players.last_name = 'Ripken' 
+  ORDER BY "year" DESC ;
