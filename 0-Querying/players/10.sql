b007d79faea8ec2b4a77bf38f6e0d46c14160523
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2024-12-19 01:24:54 +0200

    Update 10.sql

--- 0-Querying/players/10.sql
+++ 0-Querying/players/10.sql
@@ -1,1 +1,4 @@
-SELECT ROUND(AVG("height"),2) AS "Average height per each city" FROM "players" WHERE "birth_country" = 'USA' GROUP BY "birth_city" ORDER BY "Average height per each city" DESC ;
+SELECT ROUND(AVG("height"),2) AS "Average height per each city" FROM "players"
+  WHERE "birth_country" = 'USA' 
+  GROUP BY "birth_city"
+  ORDER BY "Average height per each city" DESC ;
