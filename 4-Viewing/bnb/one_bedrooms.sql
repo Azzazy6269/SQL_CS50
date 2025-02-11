aa127af2075958f39ac1b58b118d7ec43ac79ea7
Azzazy6269 <150115419+Azzazy6269@users.noreply.github.com>
2025-02-11 06:26:46 +0200

    Update one_bedrooms.sql

--- 4-Viewing/bnb/one_bedrooms.sql
+++ 4-Viewing/bnb/one_bedrooms.sql
@@ -1,4 +1,4 @@
---DROP VIEW IF EXISTS "one_bedrooms" ;
+DROP VIEW IF EXISTS "one_bedrooms" ;
 CREATE VIEW "one_bedrooms" AS
 SELECT "id" , "property_type" , "host_name" , "accommodates"
 FROM "listings"
